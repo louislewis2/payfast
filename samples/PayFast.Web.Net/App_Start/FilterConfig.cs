bf0ddf194075588428e6bce5cc37691385188804
Louis Lewis <louis@lewisonline.co.za>
2017-08-19 20:21:06 +0200

    feat: configure application insights

--- samples/PayFast.Web.Net/App_Start/FilterConfig.cs
+++ samples/PayFast.Web.Net/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace PayFast.Web.Net
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
