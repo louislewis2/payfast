8758bb6483e80147870b13ced7a13834cb1f7bba
Damien North <damien.north@savethechildren.org>
2017-10-27 16:22:38 +0100

    Changing urlencode method to use native .NET method.

--- src/PayFast/Base/PayFastBase.cs
+++ src/PayFast/Base/PayFastBase.cs
@@ -112,15 +112,8 @@
 
         protected string UrlEncode(string url)
         {
-            Dictionary<string, string> convertPairs = new Dictionary<string, string>() { { "%", "%25" }, { "!", "%21" }, { "#", "%23" }, { " ", "+" },
-            { "$", "%24" }, { "&", "%26" }, { "'", "%27" }, { "(", "%28" }, { ")", "%29" }, { "*", "%2A" }, { "+", "%2B" }, { ",", "%2C" },
-            { "/", "%2F" }, { ":", "%3A" }, { ";", "%3B" }, { "=", "%3D" }, { "?", "%3F" }, { "@", "%40" }, { "[", "%5B" }, { "]", "%5D" } };
 
-            var replaceRegex = new Regex(@"[%!# $&'()*+,/:;=?@\[\]]");
-            MatchEvaluator matchEval = match => convertPairs[match.Value];
-            string encoded = replaceRegex.Replace(url, matchEval);
-
-            return encoded;
+            return System.Net.WebUtility.UrlEncode(url.Trim()); 
         }
 
         protected string CreateHash(StringBuilder input)
