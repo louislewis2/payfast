b02aacc8b8c329daf8bd07f21cf566f473401361
Tommybearrr <33929921+Tommybearrr@users.noreply.github.com>
2019-05-13 17:35:21 +0200

    Update PayFastBase.cs

--- src/PayFast/Base/PayFastBase.cs
+++ src/PayFast/Base/PayFastBase.cs
@@ -114,9 +114,9 @@
         {
             Dictionary<string, string> convertPairs = new Dictionary<string, string>() { { "%", "%25" }, { "!", "%21" }, { "#", "%23" }, { " ", "+" },
             { "$", "%24" }, { "&", "%26" }, { "'", "%27" }, { "(", "%28" }, { ")", "%29" }, { "*", "%2A" }, { "+", "%2B" }, { ",", "%2C" },
-            { ";", "%3B" }, { "=", "%3D" }, { "?", "%3F" }, { "@", "%40" }, { "[", "%5B" }, { "]", "%5D" } };
+            { ";", "%3B" }, { "=", "%3D" }, { "?", "%3F" }, { "[", "%5B" }, { "]", "%5D" } };
 
-            var replaceRegex = new Regex(@"[%!# $&'()*+,/:;=?@\[\]]");
+            var replaceRegex = new Regex(@"[%!# $&'()*+,;=?\[\]]");
             MatchEvaluator matchEval = match => convertPairs[match.Value];
             string encoded = replaceRegex.Replace(url, matchEval);
 
